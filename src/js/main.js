6847e5e775fc0fef741d331d8425b78cc9c45263
gogromat <gogromat@gmail.com>
2015-01-18 17:10:41 -0500

    Add google analytics
    For now points to github.io ...

--- src/js/main.js
+++ src/js/main.js
@@ -45450,6 +45450,12 @@ angular.module('mgcrea.ngStrap.typeahead').run(['$templateCache', function($temp
 
 })(window, document);
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-58666276-1', 'auto');
+ga('send', 'pageview');
 angular
     .module('SmokeApp', [
         'ngAnimate',
